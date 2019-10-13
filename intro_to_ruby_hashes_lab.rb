0b863865515aedda3895181b055bca6a648be47a
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-13 23:00:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,15 +8,19 @@ end
 
 def monopoly_with_third_tier
  return  {:railroads=>
-  {
-    :pieces=>4,
+  {:pieces=>4,
+    
     :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
+    {:one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    }}}
+      :four_pieces_owned=>200},
+      
+      :names
+    
+  }
+   
+ }
 end
 
 def monopoly_with_fourth_tier
